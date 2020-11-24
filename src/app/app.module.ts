9207302e7fdd6b3720908ea6dff98fb4ced3be1a
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-11-25 00:50:59 +0200

    add component header footer currencyrate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyRateComponent } from './currency-rate/currency-rate.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyRateComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/currency-rate/currency-rate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-rate',
+  templateUrl: './currency-rate.component.html',
+  styleUrls: ['./currency-rate.component.scss']
+})
+export class CurrencyRateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
